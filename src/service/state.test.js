1728daaeba72decc7a5a50f8c0bd884d7f87f7d0
Noel Keener <noel@hedrydev.com>
2021-01-30 17:09:51 -0600

    set up 100% code coverage

--- src/service/state.test.js
+++ src/service/state.test.js
@@ -14,4 +14,20 @@ describe('state', () => {
     expect(_s$().dividend).toEqual(expected);
     expect(_s$()).toEqual(expectedState);
   });
+  test('State does not override unsupplied values', () => {
+    _s$({
+      ...INITIAL_STATE,
+      form: { premium: 500, emod: 0.975, divFactor: 5 },
+    });
+    const expectedDividend = 10000;
+    const expectedPremium = 15000;
+    const expectedState = {
+      ...INITIAL_STATE,
+      form: { premium: expectedPremium, emod: 0.975, divFactor: 5 },
+      dividend: expectedDividend,
+    };
+    State('dividend', expectedDividend);
+    State('form', { ..._s$().form, premium: expectedPremium });
+    expect(_s$()).toEqual(expectedState);
+  });
 });
